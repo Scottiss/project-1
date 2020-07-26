38c3d722d9c5d01f81631d66a3b15d0998c61f39
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-07-26 19:58:00 -0400

    added jQuery results to html div, deleted some paragraph and all p classes,  results only show text version

--- main.js
+++ main.js
@@ -33,6 +33,14 @@ $(searchBtn).click(function () {
     $.getJSON(
       `https://www.hikingproject.com/data/get-trails?lat=${coord.lat}&lon=${coord.lon}&maxDistance=10&key=${trailID}`,
       function (data) {
+        // TRAIL RESULTS DIV
+        // $("#hike-pic").html(data.trails[1].imgMedium);
+        $("#hike-name").html(data.trails[0].name);
+        $("#location").html(data.trails[1].location);
+        $("#condition-status").html(data.trails[1].conditionStatus);
+        $("#hike-type").html(data.trails[1].type);
+        $("#hike-summary").html(data.trails[1].summary);
+        $("#url").html(data.trails[1].url);
         console.log(data);
       }
     );
