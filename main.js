662ab94859e15da2bbc5d4cc49c01a9efcfe38aa
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-07-28 15:51:36 -0400

    added Bootstrap cdn, added id to button, added comments in html and deleted some extra containers

--- main.js
+++ main.js
@@ -2,15 +2,15 @@
 let weatherID = "8922c7c9ff2ae54c2727e6a09d80cc98";
 let trailID = "200845268-2016567b3b52abddf0f22a66980c383e";
 
-// HIDES WEATHER AND TRAIL DIVS
+// HIDES WEATHER AND TRAIL CONTAINERS
 $("#show-weather").hide();
 $("#show-trail").hide();
 
 // BUTTON
-$(".btn").click(function () {
+$("#btn").click(function () {
   var trailSearch = $(this).prev().val();
 
-  // SHOWS WEATHER AND TRAIL DIVS WHEN BUTTON CLICKED
+  // SHOWS WEATHER AND TRAIL CONTAINERS WHEN BUTTON CLICKED
   $("#show-trail").show();
   $("#show-weather").show();
 
